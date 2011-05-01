220b105283caa51864202aef9f726f0058333ebe
mike.kelly <mike@whatsthatweb.com.au>
2011-05-01 20:42:32 +1000

    Attempting to get the slideshow to work. It would seem there are some JS errors.

--- themes/custom/unitebusiness/template.php
+++ themes/custom/unitebusiness/template.php
@@ -1,8 +1,6 @@
 <?php
 // $Id$
 
-if (arg(2) != 'block' and arg(0) != 'admin') drupal_add_js(path_to_theme() . '/js/jquery-1.4.min.js');
-
 $style = theme_get_setting('skin');
 
 switch ($style) {
@@ -30,21 +28,25 @@ $slide = theme_get_setting('slide_shows');
 
 if (!$slide or $slide == 0) { 
 	drupal_add_js('
-		$(document).ready(function() {
-			if ($(\'#Slides\').length > 0) {
-				$(\'#Slides\').cycle({ 
-					fx: \'scrollHorz\',
-					speed: 750,
-					timeout: 6000, 
-					randomizeEffects: false, 
-					easing: \'easeOutCubic\',
-					next:   \'.slideNext\', 
-					prev:   \'.slidePrev\',
-					pager:  \'#slidePager\',
-					cleartypeNoBg: true
-				});
-			}
-		});
+    
+Drupal.behaviors.slideshow = function(context) {
+  
+  if ($(\'#Slides\').length > 0) {
+    $(\'#Slides\').cycle({ 
+      fx: \'scrollHorz\',
+      speed: 750,
+      timeout: 6000, 
+      randomizeEffects: false, 
+      easing: \'easeOutCubic\',
+      next:   \'.slideNext\', 
+      prev:   \'.slidePrev\',
+      pager:  \'#slidePager\',
+      cleartypeNoBg: true
+    });
+  }
+
+};
+
 	', 'inline');
 }
 
@@ -52,26 +54,30 @@ if ($slide == 1) {
 	drupal_add_js(path_to_theme() . '/js/galleryview/jquery.timers-1.1.2.js');
 	drupal_add_js(path_to_theme() . '/js/galleryview/jquery.galleryview-2.0-pack.js');
 	drupal_add_js('
-		$(document).ready(function() {
-			if ($(\'#GalleryView\').length > 0) {
-				$(\'#GalleryView\').galleryView({
-					show_panels: true,
-					show_filmstrip: true,
-					panel_width: 938,
-					panel_height: 340,
-					frame_width: 87,
-					frame_height: 45,
-					frame_gap: 8,
-					pointer_size: 16,
-					pause_on_hover: true,
-					filmstrip_position: \'bottom\',
-					overlay_position: \'bottom\',
-					nav_theme: \'dark\',
-					transition_speed: 800,
-					transition_interval: 4000
-				});
-			}
-		});
+    
+Drupal.behaviors.slideshow = function(context) {
+
+  if ($(\'#GalleryView\').length > 0) {
+    $(\'#GalleryView\').galleryView({
+      show_panels: true,
+      show_filmstrip: true,
+      panel_width: 938,
+      panel_height: 340,
+      frame_width: 87,
+      frame_height: 45,
+      frame_gap: 8,
+      pointer_size: 16,
+      pause_on_hover: true,
+      filmstrip_position: \'bottom\',
+      overlay_position: \'bottom\',
+      nav_theme: \'dark\',
+      transition_speed: 800,
+      transition_interval: 4000
+    });
+  }
+      
+};
+    
 	', 'inline');
 	drupal_add_css(path_to_theme() . '/css/galleryview.min.css', array('group' => CSS_THEME, 'browsers' => array('IE' => true, '!IE' => true), 'preprocess' => FALSE));
 } 
