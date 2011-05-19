4986b0e0fcb4b80e1fb7303721c649e7959934a5
mike.kelly <mike@whatsthatweb.com.au>
2011-05-19 21:58:29 +1000

    Typo in rebuld

--- rebuild.sh
+++ rebuild.sh
@@ -3,5 +3,5 @@
 rm -rf modules/contrib
 rm -rf themes/contrib
 rm -rf libraries
-http_proxy="http://localhost:3128/" drush make --working-copy --no-core --contrib-destination=. subaru.make .
+http_proxy="http://localhost:3128/" drush make --working-copy --no-core --contrib-destination=. twoofakind.make .
 # Usually run update.php and clear the cache here
