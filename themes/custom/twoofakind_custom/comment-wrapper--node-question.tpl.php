b90bb86f49b7e04c06616d59cfc80764083dc981
mike.kelly <mike@whatsthatweb.com.au>
2011-07-26 23:56:46 +1000

    Fixing some issues with the node and comment templates and also adding a comment wrapper template for question content type.

---
+++ themes/custom/twoofakind_custom/comment-wrapper--node-question.tpl.php
@@ -0,0 +1,18 @@
+<?php if ($content['comments'] || $content['comment_form']) : ?>
+
+<div id="comments">
+<div class="ribbon">
+	<div class="wrapAround"></div>
+	<div class="tab">
+		<span class="blogDate"><?php print t('Responses'); ?></span>
+		<span class="blogPostInfo"> </span>
+	</div>
+</div>
+    <?php print render($content['comments']); ?>
+	<div class="box">
+	<h2><?php print t('Post new comment'); ?></h2>
+	<?php print render($content['comment_form']); ?>
+	</div>
+</div>
+
+<?php endif; ?>
