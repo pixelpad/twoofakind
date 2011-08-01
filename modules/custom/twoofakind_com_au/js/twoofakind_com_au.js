ae46fdfc064167c9d66d34ad1442958c7c7f82b7
mike.kelly <mike@whatsthatweb.com.au>
2011-08-01 17:58:36 +1000

    Hiding some user search exposed form fields from anonymous user.

--- modules/custom/twoofakind_com_au/js/twoofakind_com_au.js
+++ modules/custom/twoofakind_com_au/js/twoofakind_com_au.js
@@ -13,11 +13,13 @@
   
   /* Show and hide the blocks
   -------------------------------------------------------------- */
-  Drupal.behaviors.expandBlocks = {
+  Drupal.behaviors.sideBarProcesing = {
     attach: function (context, settings) {
       $('div.region-sidebar-right:not(.js-processed)', context).each(function () {
         $(this).addClass('js-processed');
         var sidebar = $(this);
+        
+        /* block expansion */
         $('a.block-expand:not(.js-processed)', sidebar).each(function () {
           $(this).addClass('js-processed');
           $(this).bind('click', function(e) {
@@ -25,6 +27,16 @@
 
             return false;
           });
+        });
+        
+        /* hide labels that should not appear */
+        $('label:not(.js-processed)', sidebar).each(function () {
+          $(this).addClass('js-processed');
+          var label_for = $(this).attr('for');
+          var label_input = $('#'+label_for, sidebar);
+          if (label_input.length == 0) {
+            $(this).hide();
+          }
         });
       });
     }
