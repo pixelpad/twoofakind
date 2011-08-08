d9772f3fc1168187f6c3ed41c44ed3fcf791c9b0
mike.kelly <mike@whatsthatweb.com.au>
2011-08-08 18:44:30 +1000

    Fixing the major user actions menu and styling it within current theme.

--- themes/custom/twoofakind_custom/template.php
+++ themes/custom/twoofakind_custom/template.php
@@ -2,3 +2,48 @@
 
 // we have to add our own css here so it overwrites the previous
 drupal_add_css(path_to_theme().'/css/twoofakind-custom-style.css', array('group' => CSS_THEME));
+
+/*
+ * implements theme_menu_tree__menu_user_major_actions
+ * 
+ * @see theme_menu_tree
+ */
+function twoofakind_custom_menu_tree__menu_user_major_actions($variables) {
+  return '<div id="block-menu-user-major-actions"><ul>' . $variables['tree'] . '</ul></div>';
+}
+
+/*
+ * implements theme_menu_link__menu_user_major_actions
+ * 
+ * @see theme_menu_link
+ */
+function twoofakind_custom_menu_link__menu_user_major_actions($variables) {
+  $element = $variables['element'];
+  $sub_menu = '';
+
+  if ($element['#below']) {
+    $sub_menu = drupal_render($element['#below']);
+  }
+  
+  // add some localised options
+  if (!isset($element['#localized_options']['attributes'])) {
+    $element['#localized_options']['attributes'] = array();
+  }
+  if (!isset($element['#localized_options']['attributes']['class'])) {
+    $element['#localized_options']['attributes']['class'] = array();
+  }
+  $element['#localized_options']['attributes']['class'][] = 'btn';
+  $element['#localized_options']['html'] = TRUE;
+  
+  $output = l('<span>' . $element['#title'] . '</span>', $element['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
+
+/*
+ * implements theme_menu_tree__menu_footer_other
+ * 
+ * @see theme_menu_tree
+ */
+function twoofakind_custom_menu_tree__menu_footer_other($variables) {
+  return '<div id="block-menu-footer-other"><ul>' . $variables['tree'] . '</ul></div>';
+}
