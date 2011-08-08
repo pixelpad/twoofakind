6ce13fda5a74f52fe79f8316ebd22097c16658eb
mike.kelly <mike@whatsthatweb.com.au>
2011-08-08 17:00:52 +1000

    Tidying the block template file so it is legible and can be made sense of.

--- themes/custom/twoofakind_custom/block.tpl.php
+++ themes/custom/twoofakind_custom/block.tpl.php
@@ -1,33 +1,52 @@
 <?php
-  if ($block->module == 'wink'
-      or ($block->module == 'views' and $block->delta == '2c4bfdbb41e10bba86273c56e82003e1'))
-          { ?>
-<?php if ($block->subject): ?><h4 class="title interactive" id="open-<?php echo $block->delta; ?>"><?php print $block->subject ?></h4><?php endif;?>
-<div class="sideNavWrapper interactive" id="<?php echo $block->delta; ?>"><div class="sideNavBox-1">&nbsp;<div class="sideNavBox-2">
-<?php print $content ?>
-&nbsp;</div></div></div>
-<?php } elseif (($block->module == 'system' and $block->delta == 'navigation')
-      or ($block->module == 'user' and $block->delta == 1) or ($block->module == 'menu')
-      or ($block->module == 'menu'))
-          { ?>
+  if (
+      ($block->module == 'system' && $block->delta == 'navigation')
+      || ($block->module == 'user' && $block->delta == 1)
+//      || ($block->module == 'menu')
+     ) { ?>
+
 <?php if ($block->subject): ?><h4 class="title"><?php print $block->subject ?></h4><?php endif;?>
 <div class="sideNavWrapper"><div class="sideNavBox-1">&nbsp;<div class="sideNavBox-2">
 <?php print $content ?>
 &nbsp;</div></div></div>
-<?php } elseif (($block->module == 'montharchive' and $block->delta == 'montharchive_all') or ($block->module == 'taxonomy_blocks') or ($block->module == 'blog')){ ?>
+
+<?php }
+  elseif (
+      ($block->module == 'montharchive' && $block->delta == 'montharchive_all')
+      || ($block->module == 'taxonomy_blocks')
+      || ($block->module == 'blog')
+     ) { ?>
+
 <?php if ($block->subject): ?><h4 class="title"><?php print $block->subject ?></h4><?php endif;?>
 <div class="sideNavWrapper"><div class="sideNavBox-1">&nbsp;<div class="sideNavBox-2">
 <?php print str_replace('<ul>','<ul class="menu">',$content) ?>
 &nbsp;</div></div></div>
-<?php } elseif ($block->module == 'comment'){ ?>
+
+<?php }
+  elseif (
+      $block->module == 'comment'
+     ) { ?>
+
 <?php if ($block->subject): ?><h4 class="title"><?php print $block->subject ?></h4><?php endif;?>
 <div class="sideNavWrapper"><div class="sideNavBox-1">&nbsp;<div class="sideNavBox-2">
 <?php print preg_replace("'&nbsp;<span>.*?</span></li>'si", '</li>', str_replace(array('<ul>', 'class="active"'),array('<ul class="menu">', ''),$content)) ?>
 &nbsp;</div></div></div>
-<?php } elseif ($block->region == 'footer_1' or $block->region == 'footer_2' or $block->region == 'footer_3'){ ?>
+
+<?php }
+  elseif (
+      $block->region == 'footer_1'
+      || $block->region == 'footer_2'
+      || $block->region == 'footer_3'
+     ) { ?>
+
 <?php if ($block->subject): ?><h3><?php print $block->subject ?></h3><?php endif;?>
 <?php print $content ?>
-<?php } elseif ($block->region == 'featured_content'){ ?>
+
+<?php }
+  elseif (
+      $block->region == 'featured_content'
+     ) { ?>
+
 <div class="ribbon">
 	<div class="wrapAround"></div>
 	<div class="tab">
@@ -38,12 +57,26 @@
 <?php print $content ?>						
 <div class="clear"></div>	
 </div>
-<?php } elseif ($block->region == 'portfolio_top_txt' or $block->region == 'slide_shows' or $block->region == 'home_banner_1' or $block->region == 'home_banner_2' or $block->region == 'home_banner_3' or $block->region == 'footer'){ ?>
+
+<?php }
+  elseif (
+      $block->region == 'portfolio_top_txt'
+      || $block->region == 'slide_shows'
+      || $block->region == 'home_banner_1'
+      || $block->region == 'home_banner_2'
+      || $block->region == 'home_banner_3'
+      || $block->region == 'footer'
+     ) { ?>
+
 <?php print $content ?>
-<?php } else { ?>
+
+<?php }
+  else { ?>
+
 <?php if ($block->subject): ?><h4 class="title"><?php print $block->subject ?></h4><?php endif;?>
 <div>
 <?php print $content ?>
 </div>
+
 <?php } ?>
 <?php //print serialize($block) ?>
